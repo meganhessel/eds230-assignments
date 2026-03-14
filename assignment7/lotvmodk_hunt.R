f4d195b8350547a2544819613d2978338c45ff92
Megan Hessel <meganhessel@ucsb.edu>
2026-03-14 14:34:13 -0700

    Assignment 7 completion

--- assignment7/lotvmodk_hunt.R
+++ assignment7/lotvmodk_hunt.R
@@ -2,15 +2,13 @@
 #'
 #' function computes the rate of change of populations in a predictor prey interaction
 #' @param t  time (days)
-#' @param pop datatype list initial conditions; list with two values prey=number of prey and pred=number of predictor
-#' @param min_prey Number of prey that is necessary for hunting (default = 1)
-#' @param min_pred Number of predators that is necessary for hunting (default = 1)
+#' @param pop datatype list initial conditions; list with two values prey=number of prey and pred number of predictor
+#' @param min_prey Number of prey that is necessary for hunting (default = 50 - shop hunting when prey gets to 50)
 #' @param pars datatype list  coefficient in Lotka-Voltera pars$rprey, pars$alpha, pars$eff, par$pmort
 #'  \emph{rprey} is growth rate of prey population;
 #'  \emph{eff} is the rate of ingestion of prey by predators
 #'  \emph{alpha} is a interaction coefficient (higher values greater interaction)
 #'  \emph{h_prey} hunting rate on the prey;
-#'  \emph{h_pred} hunting rate on the predator;
 #'  \emph{pmort} mortality rate of predictor population;
 #' @examples
 #' COME BACK AND ADD EXAMPLES
@@ -21,16 +19,15 @@
 #' \item{dpred}{rate of change of preditor populutation}
 #' }
 
-pred_prey_func <- function(t, pop, pars, min_prey = 1, min_pred = 1) {
+lotvmodk_hunt <- function(t, pop, pars, min_prey = 50) {
   with(as.list(c(pars, pop)), {
     
-    # Hunt only when prey and predator populations are above thresholds (min_prey & min_pred)
-    h_prey <- ifelse(h_prey * prey < min_prey, 0, h_prey)
-    h_pred <- ifelse(h_pred * pred < min_pred, 0, h_pred)
-    
+    # Hunt only when prey populations are above the thresholds (min_prey)
+    h_prey <- ifelse(h_prey * prey > min_prey, h_prey, 0)
+
     # Scaling h_prey and h_pred with the population size -> when prey = 0, hunting = 0
-    dprey <- (rprey * (1 - prey / K) * prey) - (alpha * prey * pred) - (h_prey * prey)
-    dpred <- (eff * alpha * prey * pred) - (pmort * pred) - (h_pred * pred)
+    dprey <- (rprey * (1 - prey / K) * prey) - (alpha * prey * pred) - h_prey
+    dpred <- (eff * alpha * prey * pred) - (pmort * pred)
     
     return(list(c(dprey, dpred)))
   })
